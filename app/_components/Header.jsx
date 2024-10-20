40389762b4838e87095db6adcddd7dddb89e413e
Kazi Bilasi <kazibilasi@gmail.com>
2024-10-20 12:12:15 +0600

    authentication process done

--- app/_components/Header.jsx
+++ app/_components/Header.jsx
@@ -1,13 +1,20 @@
+"use client"
 import { Button } from "@/components/ui/button";
+import { UserButton, useUser } from "@clerk/nextjs";
+import Link from "next/link";
 import React from "react";
 
 const Header = () => {
-  return <div className="p-4 flex justify-between items-center shadow-md">
-    <img src={'./logo.svg'} alt="" />
-    <Button>
-      Get Started
-    </Button>
-  </div>;
+  const { user, isSignedIn } = useUser();
+  return (
+    <div className="p-4 flex justify-between items-center shadow-md">
+      <img src={"./logo.svg"} alt="" />
+
+      {isSignedIn ? <UserButton></UserButton> :  <Link href={'/sign-in'}><Button>Get Started</Button></Link> }
+    </div>
+
+   
+  );
 };
 
 export default Header;
--- app/_components/Hero.jsx
+++ app/_components/Hero.jsx
@@ -6,22 +6,21 @@ const Hero = () => {
       <div className="mx-auto max-w-screen-xl px-4 py-32 lg:flex lg:h-screen lg:items-center">
         <div className="mx-auto max-w-xl text-center">
           <h1 className="text-3xl font-extrabold sm:text-5xl">
-            Understand User Flow.
-            <strong className="font-extrabold text-red-700 sm:block">
+           Manage Your Expense.
+            <strong className="font-extrabold text-primary sm:block">
               {" "}
-              Increase Conversion.{" "}
+              Contron Your Money.{" "}
             </strong>
           </h1>
 
           <p className="mt-4 sm:text-xl/relaxed">
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nesciunt
-            illo tenetur fuga ducimus numquam ea!
+            Start creating your money and save ton of money.
           </p>
 
           <div className="mt-8 flex flex-wrap justify-center gap-4">
             <a
-              className="block w-full rounded bg-red-600 px-12 py-3 text-sm font-medium text-white shadow hover:bg-red-700 focus:outline-none focus:ring active:bg-red-500 sm:w-auto"
-              href="#"
+              className="block w-full rounded bg-primary px-12 py-3 text-sm font-medium text-white shadow hover:bg-primary focus:outline-none focus:ring active:bg-primary sm:w-auto"
+              href="/sign-in"
             >
               Get Started
             </a>
